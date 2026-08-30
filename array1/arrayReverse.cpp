63b91b57a8a2be09c0d964c042bc9d48c78f4a81
Ashish Kumar <ashishkumar8112008@gmail.com>
2026-08-30 15:47:30 +0530

    1

--- array1/arrayReverse.cpp
+++ array1/arrayReverse.cpp
@@ -12,12 +12,6 @@ int main(){
     }
     for(int i=n-1;i>=0;i--){
         cout<<arr[i] <<endl;
-        cout<< "Hello" <<endl;
-        cout<< "Hello" <<endl;
-        cout<< "Hello" <<endl;
-        cout<< "Hello" <<endl;
-
-        
 
     }
 }
---
+++ function/function_basic.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c;
+    cin>>a>>b>>c;
+    for(int k=1;k<=3; k++){
+        for(int i=0;i<=a;i++){
+            for(int j=1;j<=1;j++){
+                cout<<"*";
+            }
+            cout<<endl;
+        }
+    }
+
+}
